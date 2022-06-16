10732932bc173374cea1bda8b0335870b8befa31
arkadiusz971 <arkadiusz.giemza97@gmail.com>
2022-06-16 13:35:12 +0200

    create computerTurn function which returns rock, paper or scissors randomly

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+function computerTurn() {
+    const randomNumber = Math.floor((Math.random()*3))
+    switch(randomNumber) {
+        case 0:
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2:
+            return "Scissors"
+    }
+}
